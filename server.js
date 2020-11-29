534d0dd0b9ab353d823be02690a53e88595319a3
Sascha Majewsky <sascha.majewsky@pm.me>
2020-11-29 17:39:13 +0100

    Refactored realestate base route due to problems with naming

--- server.js
+++ server.js
@@ -1,27 +1,27 @@
-const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
-const morgan = require('morgan');
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const morgan = require("morgan");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
-server.use(morgan('dev'));
+server.use(morgan("dev"));
 server.use(cors());
 
-server.get('/', function rootHandler(req, res) {
+server.get("/", function rootHandler(req, res) {
   res.send(
-    `Welcome to the ${process.env.DEPLOYMENT} environment API of Immoperium!`,
+    `Welcome to the ${process.env.DEPLOYMENT} environment API of Immoperium!`
   );
 });
 
-const UsersRouter = require('./api/users/user-router.js');
-const AdvertisementsRouter = require('./api/advertisements/advertisements-router.js');
+const UsersRouter = require("./api/users/user-router.js");
+const AdvertisementsRouter = require("./api/advertisements/advertisements-router.js");
 
-server.use('/v1/users', UsersRouter);
-server.use('/v1/advertisements', AdvertisementsRouter);
+server.use("/v1/users", UsersRouter);
+server.use("/v1/realestate", AdvertisementsRouter);
 
 module.exports = server;
